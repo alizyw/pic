4e5ceb5d23f296f6ebc95ef84019dea1f095c2e1
sendfun <73876631+alizyw@users.noreply.github.com>
2022-02-27 12:45:30 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+header("Content-Type: image/jpeg;text/html; charset=utf-8");
+
+$url = $_GET["url"];
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+
+echo $response;
